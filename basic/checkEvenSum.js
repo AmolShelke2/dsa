a8dc671a368b85beddd596204ab6ab8d356ed3a2
Amol <shelkeamol897@gmail.com>
2023-06-21 22:27:36 +0530

    checkEvenSum problem statement updated

---
+++ basic/checkEvenSum.js
@@ -0,0 +1,14 @@
+/*
+You are given two numbers, stored in variables with the following names 
+
+max, min
+
+You have to find the sum of all even numbers that fall between two numbersfrom min
+ to max(including both min and max)
+
+ for example, if the value stored in max = 19, and the value stored in min = 14,
+ then the output will be computed as follows
+
+ sum of even = 14 + 16 + 18 = 48
+
+*/
