06c5294343833a9a4b95f0e1bc69761c9b3e5b70
Amol <shelkeamol897@gmail.com>
2023-06-22 19:00:50 +0530

    Find largest number statement updated

---
+++ basic/FindLargestNumber.js
@@ -0,0 +1,7 @@
+/* from given array find the largest number
+
+create a function which takes a array as an input and returns the largest number present in that array.
+
+const demoArray = [12, 35, 42, 44, 254, 224]
+
+*/
