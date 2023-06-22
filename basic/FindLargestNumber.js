63e62b69cbb91834c77c1bae59020aca847b251f
Amol <shelkeamol897@gmail.com>
2023-06-22 23:10:57 +0530

    FindLargestNumber solution updated

--- basic/FindLargestNumber.js
+++ basic/FindLargestNumber.js
@@ -5,3 +5,22 @@ create a function which takes a array as an input and returns the largest number
 const demoArray = [12, 35, 42, 44, 254, 224]
 
 */
+
+// solution
+
+const demoArray = [12, 35, 42, 44, 254, 224]
+
+const findLargestNum = (arr) => {
+  let largestNum = demoArray[0]
+
+  for (let i = 0; i <= demoArray.length; i++) {
+    if (demoArray[i] > largestNum) {
+     largestNum = demoArray[i]
+   }
+  }
+
+  return largestNum
+
+}
+
+console.log(findLargestNum(demoArray))
