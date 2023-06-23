a0be75d0d09cd9a0a68acc456ee324d4148e0012
Amol <shelkeamol897@gmail.com>
2023-06-23 23:31:38 +0530

    ConvertInchesIntoFoot solution updated

--- basic/ConvertInchesIntoFoot.js
+++ basic/ConvertInchesIntoFoot.js
@@ -3,3 +3,8 @@ for example if the input is 48 then the output should be the foots.
 
 */
 
+const convertInchesIntoFoot = (inchInput) => {
+  return inchInput / 12
+}
+
+console.log(convertInchesIntoFoot(42))
