eca1e601962f9f1d5217cd5177da01cd8b8fa570
Amol Shelke <shelkeamol897@gmail.com>
2023-06-26 22:18:59 +0530

    Update and rename checkEvenSum.js to 3. checkEvenSum.js

--- basic/3. checkEvenSum.js
+++ basic/3. checkEvenSum.js
@@ -3,7 +3,7 @@ You are given two numbers, stored in variables with the following names
 
 max, min
 
-You have to find the sum of all even numbers that fall between two numbersfrom min
+You have to find the sum of all even numbers that fall between two numbers from min
  to max(including both min and max)
 
  for example, if the value stored in max = 19, and the value stored in min = 14,
