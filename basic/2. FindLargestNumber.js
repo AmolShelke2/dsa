a0ce2ac8d947529d0679e14bd9a4ade967ce8537
Amol Shelke <shelkeamol897@gmail.com>
2023-06-26 22:18:37 +0530

    Update and rename FindLargestNumber.js to 2. FindLargestNumber.js

--- basic/2. FindLargestNumber.js
+++ basic/2. FindLargestNumber.js
@@ -1,6 +1,6 @@
-/* from given array find the largest number
+/* from the given array find the largest number
 
-create a function which takes a array as an input and returns the largest number present in that array.
+create a function which takes an array as an input and returns the largest number present in that array.
 
 const demoArray = [12, 35, 42, 44, 254, 224]
 
