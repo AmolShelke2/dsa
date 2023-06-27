89efd761dd78ab995b0956156c76f3a5c539901a
Amol <shelkeamol897@gmail.com>
2023-06-27 22:48:45 +0530

    Duplicate number solution updated

--- basic/array/2. DuplicateNum.js
+++ basic/array/2. DuplicateNum.js
@@ -1,1 +1,19 @@
 // Find Duplicate number in the array.
+
+const duplicateArray = [1, 2, 3, 4, 5, 6, 5, 7, 8]
+
+function findDuplicate(arr) {
+  const observed = {}
+
+  for (let i = 0; i < arr.length; i++) {
+    if (observed[arr[i]]) {
+      return arr[i]
+    } else {
+      observed[arr[i]] = arr[i]
+    }
+
+  }
+   return false
+}
+
+console.log(findDuplicate(duplicateArray))
