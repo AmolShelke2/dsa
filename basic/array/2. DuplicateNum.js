066af7fc5640eaef9b04bb13b16ff21456fb35fe
Amol <shelkeamol897@gmail.com>
2023-07-13 21:32:37 +0530

    commit 1

--- basic/array/2. DuplicateNum.js
+++ basic/array/2. DuplicateNum.js
@@ -1,19 +1,18 @@
 // Find Duplicate number in the array.
 
-const duplicateArray = [1, 2, 3, 4, 5, 6, 5, 7, 8]
+const duplicateArray = [1, 2, 3, 4, 5, 6, 5, 7, 8];
 
 function findDuplicate(arr) {
-  const observed = {}
+  const observed = {};
 
   for (let i = 0; i < arr.length; i++) {
     if (observed[arr[i]]) {
-      return arr[i]
+      return arr[i];
     } else {
-      observed[arr[i]] = arr[i]
+      observed[arr[i]] = arr[i];
     }
-
   }
-   return false
+  return false;
 }
 
-console.log(findDuplicate(duplicateArray))
+console.log(findDuplicate(duplicateArray));
